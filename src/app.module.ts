901fd6463427c371fd08d2ef50badcd97d6b56f9
Francisco Cordero <128191692+Francisco-cor@users.noreply.github.com>
2026-02-01 18:01:53 -0600

    feat(config): add env validation with Joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }), // Carga variables de entorno globalmente
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        NETSUITE_API_URL: Joi.string().required(),
+        PORT: Joi.number().default(3000),
+      }),
+    }),
   ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
