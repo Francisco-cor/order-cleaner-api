61c2201a7bc8983d55551b6e5c845e250c334e7e
Francisco Cordero <128191692+Francisco-cor@users.noreply.github.com>
2026-02-22 17:39:11 -0600

    infra(tf): setup basic provider & variables

---
+++ terraform/variables.tf
@@ -0,0 +1,22 @@
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "The GCP region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "The GCP zone"
+  type        = string
+  default     = "us-central1-a"
+}
+
+variable "app_name" {
+  description = "The name of the application"
+  type        = string
+  default     = "order-cleaner-api"
+}
